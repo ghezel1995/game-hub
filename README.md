5472c857315d6b335f609010d6f0eb0c8fcaf3a9
Mahsa <mahsa.ghezel.1995@gmail.com>
2024-10-20 11:13:25 +0330

    Displaying platform Icons

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 This is a responsive app which built with React, TypeScript and Chakra UI.
 For API I used [rawg API](https://www.rawg.io)
 To fetching data from the server I used Axios.
+For Icons I used react-icons.
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import React from 'react';
+import { Game } from '../hooks/useGames';
+import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react';
+import PlatformIconList from './PlatformIconList';
 
 interface Props {
-    game: Game
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
@@ -12,9 +13,13 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
+        {/* constructing an array of objects */}
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
-}
+};
 
-export default GameCard
+export default GameCard;
