122d702c349e4cfb347ee4d76de805c98f2ac4b7
Mahsa <mahsa.ghezel.1995@gmail.com>
2024-10-21 20:51:54 +0330

    handle games with no image

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -12,7 +12,7 @@ const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
     { value: '-added', label: 'Date added' },
     { value: 'name', label: 'Name' },
     { value: '-released', label: 'Release date' },
-    { value: 'metacritic', label: 'Popularity' },
+    { value: '-metacritic', label: 'Popularity' },
     { value: '-rating', label: 'Average rating' },
   ];
 
